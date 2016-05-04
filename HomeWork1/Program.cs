0b96b786f8e0b4ae080c1259ad868507387f361e
Kotemana <laradkevich@gmail.com>
2016-05-04 12:19:20 +0300

    initial

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunMenu();
+            Console.ReadLine();
+        }
+
+        private static void RunMenu()
+        {
+            
+        }
+        //devil dz
+        private static void RoboTian()
+        {
+
+        }
+
+        //Liosha dz
+        private static void PlayGame()
+        {
+
+        }
+
+        //Marta dz
+        private static void PlayWithBools()
+        {
+            
+        }
+
+        //Topa dz
+        private static void SortArray()
+        {
+            
+        }
+    }
+}
