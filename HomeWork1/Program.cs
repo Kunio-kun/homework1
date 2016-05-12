e6af862c26a4eaad064b6c1ca6d24190fe1fea52
Kunio-kun <turskiyan@gmail.com>
2016-05-12 13:55:53 +0300

    Update Program.cs

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -26,9 +26,36 @@ namespace HomeWork1
 
         //Liosha dz
         private static void PlayGame()
+	{Random generator = new Random();
+	
+    int numberhuyamber = generator.Next(1, 101);
+    int guess =-1;
+ 
+    Console.WriteLine("I am thinking of a number between 1-100.  Can you guess what it is, sick bastard?");
+	
+    while (guess != numberhuyamber);
+    {
+ 
+        if (guess < numberhuyamber);
         {
-
+            Console.WriteLine("No, sick bastard, higher ");
+ 
         }
+        if (guess > numberhuyamber);
+        {
+            Console.WriteLine("No, sick bastard, lower  ");
+            
+        }
+        if (guess == numberhuyamber)
+    
+        {
+        Console.WriteLine("Well done, sick bastard. ");
+        Console.ReadLine();
+        }
+	}
+}
+
+        
 
         //Marta dz
         private static void PlayWithBools()
